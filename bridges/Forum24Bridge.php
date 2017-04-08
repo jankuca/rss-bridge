d22eb4de5c9780b7dfe19704815013f1b80af437
jankuca <jan@jankuca.com>
2017-04-08 17:46:50 +0200

    add Forum24.cz bridge (Forum24Bridge)

---
+++ bridges/Forum24Bridge.php
@@ -0,0 +1,83 @@
+<?php
+class Forum24Bridge extends BridgeAbstract {
+
+  const MAINTAINER = 'jankuca';
+  const NAME = 'Forum24';
+  const URI = 'https://forum24.cz/';
+  const CACHE_TIMEOUT = 300; //5 min
+  const DESCRIPTION = 'Returns Forum24.cz articles';
+  const PARAMETERS = array(
+    'Rubrika Forum24' => array(
+      'rubrika_id' => array(
+        'name' => 'ID rubriky',
+        'required' => true
+      ),
+      'rubrika_name' => array(
+        'name' => 'Název rubriky',
+      )
+    )
+  );
+
+  public function collectData() {
+    $json = file_get_contents($this->getURI());
+    $sourceItems = json_decode($json);
+
+    foreach ($sourceItems as $sourceItem) {
+      $item = array();
+      $item['uri'] = $sourceItem->guid->rendered;
+      $item['title'] = $sourceItem->title->rendered;
+      $item['author'] = $sourceItem->post_author_name;
+      $item['timestamp'] = DateTime::createFromFormat(DATE_RFC3339, $sourceItem->date . '+00:00')->getTimestamp();
+
+      $content = '';
+      try {
+        $html = file_get_html($item['uri']);
+        $contentElement = $html->find('#post-content')[0];
+        foreach ($contentElement->children() as $child) {
+          if (strtolower($child->tag) === 'comment' ||
+              strtolower($child->tag) === 'script' ||
+              strtolower($child->tag) === 'ins') {
+            continue;
+          }
+
+          if (strtolower($child->tag) === 'div' && count($child->children()) === 0) {
+            continue;
+          }
+          if (strtolower($child->tag) === 'div' && $child->children(0)->tag === 'comment') {
+            continue;
+          }
+
+          if (strpos($child->{'class'}, 'article-social-links') !== FALSE ||
+              strpos($child->{'class'}, 'author-box') !== FALSE) {
+            continue;
+          }
+
+          $childHtml = (string) $child;
+          if (strpos($childHtml, 'http://forum24.cz/wp-content/uploads/2017/02/sbirka_FCM_FB.png') !== FALSE) {
+            continue;
+          }
+
+          $content .= $childHtml;
+        }
+      } catch (Exception $err) {
+        $content = $sourceItem->content;
+      }
+
+      $item['content'] = $content;
+
+      $this->items[] = $item;
+    }
+  }
+
+  public function getName() {
+    if(!is_null($this->getInput('rubrika_name'))){
+      return self::NAME . ': ' . $this->getInput('rubrika_name');
+    }
+
+    return parent::getName();
+  }
+
+  public function getURI() {
+    return self::URI . 'wp-json/wp/v2/posts?categories='.$this->getInput('rubrika_id');
+  }
+}
